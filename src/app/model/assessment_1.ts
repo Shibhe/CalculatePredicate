7e0fa70f93ec798dedb0011f20da6ac4f8250d0d
Joseph Sibiya <sibiyajvt@gmail.com>
2018-05-27 19:34:14 +0200

    Changes

--- src/app/model/assessment_1.ts
+++ src/app/model/assessment_1.ts
@@ -1,6 +1,18 @@
 export class Assessment1 {
-    t1: number;
-    t2: number;
-    t3: number;
-    t4: number;
+    t1: number = 0;
+    t2: number = 0;
+    t3: number = 0;
+    t4: number = 0;
+    t5: number = 0;
+    t6: number = 0;
+    t7: number = 0;
+    t8: number = 0;
+    s1: string;
+    s2: string;
+    s3: string;
+    s4: string;
+    s5: string;
+    s6: string;
+    s7: string;
+    s8: string;
 }
